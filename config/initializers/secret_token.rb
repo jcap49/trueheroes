23c88554bb9b18d95f2f4038dbc84724219f615e
jcap49 <jcapecelatro@gmail.com>
2013-04-29 13:52:36 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrueHeroes::Application.config.secret_token = 'd4d35c55e8dd7c2eefff13a85260e06a60cac65eb391e595787e351650115d5f997ec18f6b88bbedb16b3b38ca9ca01da7645f05021df7839adc518afcfe1bba'
