895304478cb72c3c5b0010da868e2b58db8743c3
jcap49 <jcapecelatro@gmail.com>
2013-05-02 10:34:22 -0700

    style changes; additions to pledge model

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,3 +1,13 @@
 class Pledge < ActiveRecord::Base
+
+  #attrs
   attr_accessible :activity, :date, :first_name, :last_name, :email
+
+  #validators
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :date, presence: true
+  validates :activity, presence: true
+  validates :email, presence: true
+
 end
