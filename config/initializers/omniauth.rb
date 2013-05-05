8878482c141c0591b35ed4ac168ac16fe15ba03c
jcap49 <jcapecelatro@gmail.com>
2013-05-05 16:54:26 -0700

    basic oauth implemented

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 if Rails.env == 'development' || Rails.env == 'test'
   Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV["FACEBOOK_DEV"], ENV["FACEBOOK_DEV_SECRET"]
-    provider :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"]
+    provider :facebook, "483104801760721", "7e581795f88fbb34c10782be4dfbf499"
+    provider :twitter, "J1nYi1ddOI8YVYyartye8A", "kQtGqAJVilPzPIgA4nXbolylbAJj0SP04xhEJSbk8"
   end
 else
   # Production
